a757c217e20eb7b4e0d76e403af362a052bae037
umuhozalionel <muhozalionel@gmail.com>
2024-11-02 13:34:04 +0000

    Initial commit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
-"""Unittest module for access_nested_map."""
+"""Unittest module for get_json function."""
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import get_json
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Tests for the access_nested_map function"""
-
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map function"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+class TestGetJson(unittest.TestCase):
+    """Tests for the get_json function"""
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """Test KeyError exception in access_nested_map function"""
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
-        self.assertEqual(cm.exception.args[0], path[-1])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test get_json function"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        response = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, test_payload)
 
 if __name__ == "__main__":
     unittest.main()
