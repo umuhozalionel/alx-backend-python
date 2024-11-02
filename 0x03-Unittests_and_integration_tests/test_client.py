c490be12ab58771d3d1ec8069cf71aa05bae2266
umuhozalionel <muhozalionel@gmail.com>
2024-11-02 15:54:30 +0000

    Initial

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -39,6 +39,18 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """Tear down class method to stop the patcher"""
         cls.get_patcher.stop()
 
+    def test_public_repos(self):
+        """Test GithubOrgClient.public_repos"""
+        test_instance = GithubOrgClient("google")
+        result = test_instance.public_repos()
+        self.assertEqual(result, self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Test GithubOrgClient.public_repos with license filter"""
+        test_instance = GithubOrgClient("google")
+        result = test_instance.public_repos(license="apache-2.0")
+        self.assertEqual(result, self.apache2_repos)
+
 
 if __name__ == "__main__":
     unittest.main()
