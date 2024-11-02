6cbf734eb34dfc4e219feb54d51dea0851217236
umuhozalionel <muhozalionel@gmail.com>
2024-11-02 14:10:51 +0000

    Initial commit

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unittest module for GithubOrgClient."""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -23,6 +23,15 @@ class TestGithubOrgClient(unittest.TestCase):
         response = test_instance.org
         self.assertEqual(response, {"org": org_name})
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test GithubOrgClient._public_repos_url"""
+        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        test_instance = GithubOrgClient("google")
+        
+        result = test_instance._public_repos_url
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
 
 
 if __name__ == "__main__":
