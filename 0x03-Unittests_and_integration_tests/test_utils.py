b8b9660f008798c68965104a46a4cb7d0a20c807
umuhozalionel <muhozalionel@gmail.com>
2024-11-02 13:28:08 +0000

    Initial commit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,5 +17,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test KeyError exception in access_nested_map function"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(cm.exception.args[0], path[-1])
+
 if __name__ == "__main__":
     unittest.main()
