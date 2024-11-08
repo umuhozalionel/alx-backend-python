85e89ee5b6e736b7d1023185b2bce85744fc6dc9
umuhozalionel <muhozalionel@gmail.com>
2024-11-08 21:04:01 +0000

    Initial commit

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -66,13 +66,26 @@ class TestGithubOrgClient(unittest.TestCase):
             )
 
             client = GithubOrgClient('google')
-            result = client.public_repos()  # Ensure we are calling the method
+            result = client.public_repos()
 
             self.assertEqual(result, ["repo1", "repo2", "repo3"])
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with(
                 'https://api.github.com/orgs/google/repos'
             )
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test that has_license returns the expected value
+        """
+        client = GithubOrgClient('google')
+        result = client.has_license(repo, license_key)
+
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
